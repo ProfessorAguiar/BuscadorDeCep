437e4d27883e27b2e413a0ca0d74ff35c93e4a0e
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-05-15 19:43:00 -0300

    Final

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -6,27 +6,31 @@ import axios from 'axios'
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
-  rua!: string
-  bairro: string = ''
-  cidade: string = ''
-  estado: string = ''
-  cep: string = ''
+  isResult:boolean = false
+  rua!:string
+  bairro!:string
+  cidade!:string
+  estado!:string
+  cepNum!:string
   buscarCEP(value: string) {
-    const endereco = {
-      rua: '',
-      bairro: '',
-      cidade: '',
-      estado: '',
-      cep: '',
-    }
-    const options = { method: 'GET', url: `http://viacep.com.br/ws/${value}/json/` };
-    axios.request(options).then(function (response) {
-      endereco.rua=(response.data.logradouro);
-      console.log(response.data.bairro);
-      console.log(response.data.localidade);
-      console.log(response.data.uf);
+    const options = { 
+      method: 'GET', 
+      url: `http://viacep.com.br/ws/${value}/json/` 
+    };
+    axios.request(options).then((response)=>{
+      this.rua = (response.data.logradouro);
+      this.bairro = (response.data.bairro);
+      this.cidade = (response.data.localidade);
+      this.estado = (response.data.uf);
+      this.cepNum=value
+      console.log(value)
+      this.isResult=true
     }).catch(function (error) {
       console.error(error);
     });
+  }
+  limparCEP(){
+    this.isResult=false
+    this.cepNum=''
   }
 }
